9b77b8a3746e3f6de88adb661b5826eabebec411
SurplusSineWaves <surplussinewaves@gmail.com>
2026-04-15 13:15:02 +0100

    Colour strings moves

---
+++ src/ColourStrings.hs
@@ -0,0 +1,22 @@
+{- |
+Module      : ColourStrings
+Description : Helper functions for colouring output
+License     : BSD-3-Clause
+Maintainer  : surplussinewaves@gmail.com
+-}
+module ColourStrings where
+
+toRed :: String -> String
+toRed s = "\o33[31m" ++ s ++ "\o33[0m"
+
+toBlue :: String -> String
+toBlue s = "\o33[34m" ++ s ++ "\o33[0m"
+
+toMagenta :: String -> String
+toMagenta s = "\o33[35m" ++ s ++ "\o33[0m"
+
+toCyan :: String -> String
+toCyan s = "\o33[36m" ++ s ++ "\o33[0m"
+
+toGreen :: String -> String
+toGreen s = "\o33[32m" ++ s ++ "\o33[0m"
