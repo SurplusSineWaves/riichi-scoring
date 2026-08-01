04fe38f30ce1c5c680921f8ce47b88726f4cdb1a
SurplusSineWaves <surplussinewaves@gmail.com>
2026-08-01 10:23:03 +0100

    Created new file for Shanten calculations. Implement in version 0.6.0.0

---
+++ src/Riichi/Shanten.hs
@@ -0,0 +1,7 @@
+{- |
+Module      : Riichi.Shanten
+Description : Datatypes representing tiles and associated functions.
+License     : BSD-3-Clause
+Maintainer  : surplussinewaves@gmail.com
+-}
+module Riichi.Shanten where
